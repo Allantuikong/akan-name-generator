d6011c565f16c927538b3feaec480e236255483e
[Allantuikong] <allantuix@gmail.com>
2020-07-19 14:53:59 +0300

    Add variables for Akan names and corresponding dates

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Akan names 
+let maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+
+let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+// Getting day of birth from date given
+let date = function(){
+    document.getElementById("dob1").value
+}
+
