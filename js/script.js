83675d3cec37fb3acc7a12762f50d654ab1191f5
[Allantuikong] <allantuix@gmail.com>
2020-07-19 16:47:37 +0300

    Add functions to compute day of week from given date

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@ let maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 let femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
 // Getting day of birth from date given
-let date = function(){
-    document.getElementById("dob1").value
+
+let dob= function(){
+    document.getElementById("date").value;
+} 
+
+function calculate(){ 
+    let givenDate=dob()
+    dayOfTheWeek= givenDate.getDay()
 }
+
+let result = function () {
+    dayOfTheWeek = calculate()
+}
+
+
+
 
